9f8193934dcbf7f050d973471312801ecf495535
cubingkid123 <tdshoaff@gmail.com>
2024-02-02 13:35:13 -0700

    initial commit

---
+++ data_cleaning.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import cross_val_score
+
+
+data_file_path = "raw_data.csv"
+data = pd.read_csv(data_file_path)
+
+features = ['Extraverted', 
+            'Intuitive', 
+            'Thinking', 
+            'Judging', 
+            'Assertive', 
+            'SAT', 
+            'Age', 
+            'School Year', 
+            'Sex']
+
+targets = ['Math', 
+           'English', 
+           'Science', 
+           'Art', 
+           'History']
+
+
+x = data[features]
+y = data.Math
+
+x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.9, random_state=0)
+
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+obj = (x_train.dtypes == 'object')
+num = (x_train.dtypes == 'float64')
+object_cols = list(obj[obj].index)
+number_cols = list(num[num].index)
+print(object_cols)
+print(number_cols)
+
+
+# object_cols = [cname for cname in x_train.columns if
+#                     x_train[cname].nunique() < 10 and 
+#                     x_train[cname].dtype == "object"]
+
+# number_cols = [cname for cname in x_train.columns if 
+#                 x_train[cname].dtype in ['int64', 'float64']]
+
+# my_cols = object_cols + number_cols
+# X_train = x_train[my_cols].copy()
+# X_valid = x_valid[my_cols].copy()
+
+numerical_transformer = SimpleImputer(strategy='constant')
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, number_cols),
+        ('cat', categorical_transformer, object_cols)
+    ])
+
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('model', model)
+                             ])
+
+my_pipeline.fit(x_train, y_train)
+
+preds = my_pipeline.predict(x_valid)
+
+score = mean_absolute_error(y_valid, preds)
+print('MAE:', str(score))
---
+++ test_model.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error as mae
+import tensorflow as tf
+
+
+data_file_path = "playing_data.csv"
+data = pd.read_csv(data_file_path)
+
+# print(data.columns)
+
+# bad_columns = ["Unnamed: 13", "Unnamed: 22", "Unnamed: 23", "Unnamed: 24", "Unnamed: 25", "Unnamed: 26", "Unnamed: 27"]
+# for column in bad_columns:
+#     data = data.drop(column, axis=1)
+
+data = data.drop("Unnamed: 13", axis=1)
+
+# features = ('Extraverted', 'Intuitive', 'Thinking', 'Judging', 'Assertive', 'Math',
+#             'English', 'Science', 'Art', 'History', 'SAT', 'SAT Math',
+#             'SAT Reading', 'ACT', 'ACT English', 'ACT Math', 'ACT Reading',
+#             'ACT Science', 'Age', 'School Year', 'Sex')
+
+testing_features = ['Extraverted', 'Intuitive', 'Thinking', 'Judging', 'Assertive', 'Age', 'School Year']
+
+targets = ['Math', 
+           'English', 
+           'Science', 
+           'Art', 
+           'History']
+
+y = data.English
+X = data[testing_features]
+
+x_train, x_val, y_train, y_val = train_test_split(X, y, train_size=0.7, random_state=0)
+
+model = RandomForestRegressor(random_state=1)
+
+model.fit(x_train, y_train)
+
+val_mae = mae(y_val, model.predict(x_val))
+
+print("MAE: {}".format(val_mae))
