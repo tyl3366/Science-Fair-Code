b07554a843f58ae7ab0ada7f4a6ac3327bb64c34
cubingkid123 <tdshoaff@gmail.com>
2024-02-13 15:47:09 -0700

    model v2

--- model_v2.py
+++ model_v2.py
@@ -17,7 +17,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.neural_network import MLPRegressor
 
 # Import data
-data_file_path = "clean_data.csv"
+data_file_path = "clean_data_v2.csv"
 data = pd.read_csv(data_file_path)
 
 # Features to be used for prediction
@@ -26,17 +26,9 @@ features = ['Extraverted',
             'Thinking', 
             'Judging',
             'Assertive',
+            'SAT',
             '7',
             '8']
-
-# 'SAT'
-# '0'
-# '1'
-# '2'
-# '3'
-# '4'
-# '5'
-# '6'
 
 # Predicting targets
 targets = ['Math', 
@@ -53,29 +45,36 @@ y = data[targets]
 x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.9, random_state=0)
 
 # Neural Network Regressor
-# MAE: 7.06
+# MAE: 1.15
 # model = MultiOutputRegressor(MLPRegressor(random_state=0, max_iter=500)).fit(x_train, y_train)
 
-# Random Forest Model
-# MAE: 4.76
-# model = MultiOutputRegressor(RandomForestRegressor(n_estimators=100, random_state=0)).fit(x_train, y_train)
-
 # Extra Trees Regressor
-# MAE: 4.64
+# MAE: 1.02
 # model = MultiOutputRegressor(ExtraTreesRegressor(n_estimators=500, min_samples_split=3, random_state=0)).fit(x_train, y_train)
 
 # Bagging Regressor
-# MAE: 4.62
+# MAE: 1.02
 # model = MultiOutputRegressor(BaggingRegressor(n_estimators=100, random_state=0, max_samples=5)).fit(x_train, y_train)
 
 # Gradient Boosting Regressor
-# MAE: 4.61
+# MAE: 1.02
 # model = MultiOutputRegressor(GradientBoostingRegressor(n_estimators=5, learning_rate=0.01, random_state=0)).fit(x_train, y_train)
 
 # AdaBoost Regressor 
-# MAE: 4.52
-model = MultiOutputRegressor(AdaBoostRegressor(learning_rate=0.001, random_state=0)).fit(x_train, y_train)
-# model = AdaBoostRegressor(learning_rate=0.001, random_state=0).fit(x_train, y_train)
+# MAE: 1.04
+# model = MultiOutputRegressor(AdaBoostRegressor(learning_rate=0.001, random_state=0)).fit(x_train, y_train)
+
+# Random Forest Model
+# MAE: 1.05
+# model = RandomForestRegressor(n_estimators=100, random_state=0).fit(x_train, y_train)
+model = MultiOutputRegressor(RandomForestRegressor(n_estimators=100, random_state=0)).fit(x_train, y_train)
+
+# Math MAE: 1.09 = %78.2
+# English MAE: 1.23 = %75.4
+# Science MAE: 1.17 = %76.6
+# Art MAE: 0.46 = %90.8
+# History MAE: 1.29 = %74.2
+# Overall MAE: 1.05 = %79
 
 # Create pipline for cross validation
 pipeline = Pipeline(steps=[('model', model)])
@@ -85,7 +84,3 @@ scores = -1 * cross_val_score(pipeline, x, y, cv=5, scoring='neg_mean_absolute_e
 
 # Print validation score
 print("MAE: " + str(scores.mean()))
-
-sant = ([[68,	30,	25,	43,	17,	1,	0]])
-
-print(model.predict(sant))
